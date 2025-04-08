5d1538c8807b81973862f83a4d7e8e313f7bf305
roy-edwin-ics4u1-03-25 <roy.edwin@student.tdsb.on.ca>
2025-04-08 07:41:21 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,8 +1,17 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
-	}
-
-
+public static void main(String args[]) {
+}
+    
+public static void insertionSort(char[] characters) {
+        for (int x = 1; x < characters.length; x++) {
+        char key = characters[x];  
+        int y = x - 1;
+	while (y >= 0 && characters[y] > key) {
+        characters[y + 1] = characters[y];
+        y = y - 1;
+		}
+	characters[y + 1] = key;
+        }
+    }
 }
